47fdd672c88b8d62e46e3cbf847ecdd1d60343e2
vacodarian <73001436+vacodarian@users.noreply.github.com>
2021-01-10 22:47:57 +0100

    my second codechange

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("halla");
+        String myName = "hva skjer a";
+	    System.out.println(myName);
     }
 }
